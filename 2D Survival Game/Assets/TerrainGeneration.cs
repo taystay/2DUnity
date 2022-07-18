73e48ff93532c6f22482173b3fe6cdbebc9cdb0d
Taylor Liu <liu.7891@buckeyemail.osu.edu>
2022-07-17 21:16:48 -0400

    Tree Generation Added
    Video 2 finished

--- 2D Survival Game/Assets/TerrainGeneration.cs
+++ 2D Survival Game/Assets/TerrainGeneration.cs
@@ -1,17 +1,37 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    public Sprite tile;
+    [Header("Tile Sprites")]
+    public Sprite dirt;
+    public Sprite grass;
+    public Sprite stone;
+    public Sprite log;
+    public Sprite logBase;
+    public Sprite leaf;
+
+    [Header("Generation Settings")]
     public int worldSize = 100;
+    public int dirtLayerHeight = 5;
+    public bool generateCave = true;
+    public int heightAddition = 25;
     public float surfaceVal = 0.25f;
-    public float caveFreq = 0.08f;
-    public float terrFreq = 0.04f;
-    public float heightMultiplier = 25;
-    public float heightAddition = 25f;
+    public float heightMultiplier = 4f;
+
+    [Header("Tree Generation")]
+    public int treeChance = 10;
+    public int minTreeHeight = 3;
+    public int maxTreeHeight = 7;
+
+    [Header("Noise Settings")]
+    public float caveFreq = 0.05f;
+    public float terrFreq = 0.05f;
     public float seed;
     public Texture2D noiseTexture;
+
+    private List<Vector2> worldTiles = new List<Vector2>();
 
     private void Start() {
         seed = Random.Range(-10000, 10000);
@@ -23,12 +43,29 @@ public class TerrainGeneration : MonoBehaviour
         for (int x = 0; x < worldSize; x++) {
             float height = Mathf.PerlinNoise((x + seed) * terrFreq, seed * terrFreq) * heightMultiplier + heightAddition;
             for (int y = 0; y < height; y++) {
-                if (noiseTexture.GetPixel(x, y).r < surfaceVal) {
-                    GameObject newTile = new GameObject(name = "tile");
-                    newTile.transform.parent = transform;
-                    newTile.AddComponent<SpriteRenderer>();
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
-                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                Sprite tileSprite;
+                if (y < height - dirtLayerHeight) {
+                    tileSprite = stone;
+                } else if (y < height - 1) {
+                    tileSprite = dirt;
+                } else {
+                    tileSprite = grass;
+                }
+
+                if(generateCave) {
+                    if (noiseTexture.GetPixel(x, y).r > surfaceVal) {
+                        PlaceTile(tileSprite, x, y);
+                    }
+                } else {
+                    PlaceTile(tileSprite, x, y);
+                }
+
+                if (y >= height - 1) {
+                    int t = Random.Range(0, treeChance);
+                    if (t == 1) {
+                        if(worldTiles.Contains(new Vector2(x, y))) 
+                            GenerateTree(x, y + 1);
+                    }
                 }
             }
         }
@@ -45,5 +82,37 @@ public class TerrainGeneration : MonoBehaviour
         }
 
         noiseTexture.Apply();
+    }
+
+    public void GenerateTree(float x, float y) {
+        int treeH = Random.Range(minTreeHeight, maxTreeHeight);
+        for (int h = 0; h < treeH; h++) {
+            if(h == 0) {
+                PlaceTile(logBase, x, y + h);
+            } else {
+                PlaceTile(log, x, y + h);
+            }
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if(j < 2) {
+                PlaceTile(leaf, x, y + treeH + j);
+                PlaceTile(leaf, x + 1, y + treeH + j);
+                PlaceTile(leaf, x - 1, y + treeH + j);
+            } else {
+                PlaceTile(leaf, x, y + treeH + j);
+            }
+        }
+    }
+
+    public void PlaceTile(Sprite tileSprite, float x, float y) {
+        GameObject newTile = new GameObject();
+        newTile.transform.parent = transform;
+        newTile.AddComponent<SpriteRenderer>();
+        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+        newTile.name = tileSprite.name;
+        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+
+        worldTiles.Add(newTile.transform.position - (Vector3.one * 0.5f));
     }
 }
