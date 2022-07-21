f7d12b033abe4cdbdcf8e7e10401cea8381f8931
Taylor Liu <liu.7891@buckeyemail.osu.edu>
2022-07-20 21:48:14 -0400

    Completed biomes
    Video 7 finished

--- 2D Survival Game/Assets/Scripts/BiomeClass.cs
+++ 2D Survival Game/Assets/Scripts/BiomeClass.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class BiomeClass
 {
     public string name;
+    public TileAtlas tileAtlas;
     public Color biomeColor;
 
     [Header("Noise Settings")]
--- 2D Survival Game/Assets/Scripts/TerrainGeneration.cs
+++ 2D Survival Game/Assets/Scripts/TerrainGeneration.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
-
     [Header("Tile Atlas")]
     public TileAtlas tileAtlas;
     public float seed;
@@ -13,7 +12,9 @@ public class TerrainGeneration : MonoBehaviour
 
     [Header("Biomes")]
     public float biomeFreq;
-    public Gradient biomeColors;
+    public Gradient biomeGradient;
+    public GradientColorKey[] biomeColorKey;
+    public GradientAlphaKey[] biomeAlphaKey;
     public Texture2D biomeMap;
 
     [Header("Generation Settings")]
@@ -43,15 +44,80 @@ public class TerrainGeneration : MonoBehaviour
 
     private GameObject[] worldChunks;
     private List<Vector2> worldTiles = new List<Vector2>();
+    public int[,] biomeList;
 
     private void OnValidate() {
+        SetGradient();
         DrawTextures();
     }
 
     private void Start() {
+        SetGradient();
         DrawTextures();
+        GetTileBiome();
         CreateChunks();
         GenerateTerrain();
+    }
+
+    public void SetGradient() {
+        biomeGradient = new Gradient();
+        biomeGradient.mode = GradientMode.Fixed;
+
+        //Gradient color keys
+        biomeColorKey = new GradientColorKey[4];
+        biomeColorKey[0].color = biomes[0].biomeColor;
+        biomeColorKey[1].color = biomes[1].biomeColor;
+        biomeColorKey[2].color = biomes[2].biomeColor;
+        biomeColorKey[3].color = biomes[3].biomeColor;
+
+        //Gradient color time keys
+        biomeColorKey[0].time = 0.25f;
+        biomeColorKey[1].time = 0.50f;
+        biomeColorKey[2].time = 0.75f;
+        biomeColorKey[3].time = 1.0f;
+
+        //Gradient alpha keys
+        biomeAlphaKey = new GradientAlphaKey[2];
+        biomeAlphaKey[0].alpha = 1.0f;
+        biomeAlphaKey[1].alpha = 1.0f;
+
+        //Gradient time keys
+        biomeAlphaKey[0].time = 0.0f;
+        biomeAlphaKey[1].time = 1.0f;
+
+        biomeGradient.SetKeys(biomeColorKey, biomeAlphaKey);
+    }
+
+    public int GetCurrTileBiome(int x, int y) {
+        int biome = -1;
+
+        //Debug.Log("biome map: " + biomeMap.GetPixel(x, y).r + " " + biomeMap.GetPixel(x, y).g + " " + biomeMap.GetPixel(x, y).b);
+
+        if ((Mathf.Abs(biomeMap.GetPixel(x, y).r - biomes[0].biomeColor.r) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).g - biomes[0].biomeColor.g) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).b - biomes[0].biomeColor.b) < 0.1f)) {
+            biome = 0;
+        } else if ((Mathf.Abs(biomeMap.GetPixel(x, y).r - biomes[1].biomeColor.r) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).g - biomes[1].biomeColor.g) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).b - biomes[1].biomeColor.b) < 0.1f)) {
+            biome = 1;
+        } else if ((Mathf.Abs(biomeMap.GetPixel(x, y).r - biomes[2].biomeColor.r) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).g - biomes[2].biomeColor.g) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).b - biomes[2].biomeColor.b) < 0.1f)) {
+            biome = 2;
+        } else if ((Mathf.Abs(biomeMap.GetPixel(x, y).r - biomes[3].biomeColor.r) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).g - biomes[3].biomeColor.g) < 0.1f) && (Mathf.Abs(biomeMap.GetPixel(x, y).b - biomes[3].biomeColor.b) < 0.1f)) {
+            biome = 3;
+        }
+
+        return biome;
+    }
+
+    public void GetTileBiome() {
+        biomeList = new int[worldSize, worldSize];
+        Debug.Log("biome grass: " + biomes[0].biomeColor.r + " " + biomes[0].biomeColor.g + " " + biomes[0].biomeColor.b);
+        Debug.Log("biome forest: " + biomes[1].biomeColor.r + " " + biomes[1].biomeColor.g + " " + biomes[1].biomeColor.b);
+        Debug.Log("biome tundra: " + biomes[2].biomeColor.g + " " + biomes[2].biomeColor.g + " " + biomes[2].biomeColor.b);
+        Debug.Log("biome desert: " + biomes[3].biomeColor.r + " " + biomes[3].biomeColor.g + " " + biomes[3].biomeColor.b);
+        for (int x = 0; x < worldSize; x++) {
+            for (int y = 0; y < worldSize; y++) {
+                biomeList[x, y] = GetCurrTileBiome(x, y);
+                //Debug.Log(x + " " + y + " " + biomeList[x, y]);
+            }
+        }
     }
 
     public void DrawTextures() {
@@ -77,8 +143,11 @@ public class TerrainGeneration : MonoBehaviour
     public void DrawBiomeTexture() {
         for (int x = 0; x < biomeMap.width; x++) {
             for (int y = 0; y < biomeMap.height; y++) {
+                //X and Y
                 float v = Mathf.PerlinNoise((x + seed) * biomeFreq, (y + seed) * biomeFreq);
-                Color col = biomeColors.Evaluate(v);
+                //Just X
+                //float v = Mathf.PerlinNoise((x + seed) * biomeFreq, (x + seed) * biomeFreq);
+                Color col = biomeGradient.Evaluate(v);
                 biomeMap.SetPixel(x, y, col);
             }
         }
@@ -90,20 +159,27 @@ public class TerrainGeneration : MonoBehaviour
         int numChunks = worldSize / chunkSize;
         worldChunks = new GameObject[numChunks];
         for (int i = 0; i < numChunks; i++) {
-            GameObject newChunk = new GameObject();
-            newChunk.name = "Chunk " + i.ToString();
+            GameObject newChunk = new GameObject {
+                name = "Chunk " + i.ToString()
+            };
             newChunk.transform.parent = transform;
             worldChunks[i] = newChunk;
         }
     }
-
+    
     public void GenerateTerrain() {
+        Sprite[] tileSprite;
         for (int x = 0; x < worldSize; x++) {
-            float height = Mathf.PerlinNoise((x + seed) * terrFreq, seed * terrFreq) * heightMultiplier + heightAddition;
+            int curBiome = biomeList[x, 0];
+            float height = Mathf.PerlinNoise((x + seed) * biomes[curBiome].terrFreq, seed * biomes[curBiome].terrFreq) * biomes[curBiome].heightMultiplier + heightAddition;
             for (int y = 0; y < height; y++) {
-                Sprite[] tileSprite;
+                curBiome = biomeList[x, y];
                 if (y < height - dirtLayerHeight) {
-                    tileSprite = tileAtlas.stone.tileSprites;
+                    if(curBiome == -1) {
+                        tileSprite = tileAtlas.stone.tileSprites;
+                    } else {
+                        tileSprite = biomes[curBiome].tileAtlas.stone.tileSprites;
+                    }
 
                     if (ores[0].spreadTexture.GetPixel(x, y).r > 0.5f && height - y > ores[0].maxSpawnHeight)
                         tileSprite = tileAtlas.coal.tileSprites;
@@ -114,12 +190,22 @@ public class TerrainGeneration : MonoBehaviour
                     if (ores[3].spreadTexture.GetPixel(x, y).r > 0.5f && height - y > ores[3].maxSpawnHeight)
                         tileSprite = tileAtlas.diamond.tileSprites;
                 } else if (y < height - 1) {
-                    tileSprite = tileAtlas.dirt.tileSprites;
+                    if (curBiome == -1) {
+                        tileSprite = tileAtlas.dirt.tileSprites;
+                    }
+                    else {
+                        tileSprite = biomes[curBiome].tileAtlas.dirt.tileSprites;
+                    }
                 } else {
-                    tileSprite = tileAtlas.grass.tileSprites;
+                    if (curBiome == -1) {
+                        tileSprite = tileAtlas.grass.tileSprites;
+                    }
+                    else {
+                        tileSprite = biomes[curBiome].tileAtlas.grass.tileSprites;
+                    }
                 }
 
-                if(generateCave) {
+                if(biomes[curBiome].generateCave) {
                     if (caveNoiseTexture.GetPixel(x, y).r > 0.5f) {
                         PlaceTile(tileSprite, x, y);
                     }
@@ -127,7 +213,7 @@ public class TerrainGeneration : MonoBehaviour
                     PlaceTile(tileSprite, x, y);
                 }
 
-                if (y >= height - 1) {
+                if (y >= height - 1 && curBiome != 2 && curBiome != 3) {
                     int t = Random.Range(0, treeChance);
                     if (t == 1) {
                         if(worldTiles.Contains(new Vector2(x, y))) 
@@ -183,8 +269,7 @@ public class TerrainGeneration : MonoBehaviour
 
         GameObject newTile = new GameObject();
 
-        float chunkCoord = Mathf.Round(x / chunkSize) * chunkSize;
-        chunkCoord /= chunkSize;
+        float chunkCoord = Mathf.Round(x / chunkSize);
         newTile.transform.parent = worldChunks[(int)chunkCoord].transform;
 
         newTile.AddComponent<SpriteRenderer>();
